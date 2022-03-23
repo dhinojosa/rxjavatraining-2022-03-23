1d0f77dfb44dcf5c8dbaecefd751bd2bddd202ae
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-03-23 17:10:34 -0600

    Created all the different ways we can create an Observable same goes for Flowable

--- src/test/java/com/xyzcorp/instructor/ObservableTest.java
+++ src/test/java/com/xyzcorp/instructor/ObservableTest.java
@@ -1,12 +1,20 @@
 package com.xyzcorp.instructor;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
 import org.junit.Test;
+
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ObservableTest {
 
@@ -71,6 +79,102 @@ public class ObservableTest {
     }
 
     public void debugComplete(String label) {
-        System.out.printf("%s (On Complete: [%s]\n", label, Thread.currentThread());
+        System.out.printf("%s (On Complete: [%s]\n", label,
+            Thread.currentThread());
     }
+
+    @Test
+    public void testDoOnNext() throws InterruptedException {
+        //Predicate: x -> (true | false)
+        //Function: x -> y
+        //Identity Function: x -> x (Not a java standard)
+        //Consumer: x -> void
+        //Supplier: void -> x
+
+        Observable<String> stringObservable = Observable
+            .just(10, 40, 320, 100, 0 , 19, 600)
+            .doOnNext(integer -> debug("i1", integer))
+            .map(x -> 100 / x)
+            .doOnNext(integer -> debug("i2", integer))
+            .map(integer -> integer + 1 + "!");
+
+
+        stringObservable.retry(4)
+            .subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("Done"));
+
+        System.out.println("----------");
+
+        stringObservable.subscribe(x -> {
+                if (Objects.equals(x, "20!"))
+                    throw new RuntimeException("I don't like that number");
+                System.out.println(x);
+            }, Throwable::printStackTrace,
+            () -> System.out.println("Done"));
+
+
+        System.out.println("----------");
+
+        stringObservable
+            .subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testCreateWithFuture() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Observable<Integer> integerObservable =
+            Observable.fromFuture(executorService.submit(() -> 50 * 60));
+    }
+
+    @Test
+    public void testCreateRange() {
+        Observable.range(1, 5).subscribe(System.out::println);
+        Observable.range(0, 5).subscribe(System.out::println);
+        Observable.range(10, 20).subscribe(System.out::println);
+    }
+
+    @Test
+    public void testCreateIterable() {
+        Observable<Integer> integerObservable =
+            Observable.fromIterable(Arrays.asList(30, 10, 500, 12));
+    }
+
+    @Test
+    public void testInterval() throws InterruptedException {
+        Observable.interval(2, TimeUnit.SECONDS)
+            .doOnNext(i -> debug("i1", i))
+            .subscribe(System.out::println);
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testDefer() throws InterruptedException {
+        Observable<ZonedDateTime> zonedDateTimeObservable =
+            Observable.defer(() -> Observable.just(ZonedDateTime.now()));
+
+        zonedDateTimeObservable.subscribe(s -> System.out.printf("S1: %s\n", s));
+
+        Thread.sleep(4000);
+
+        zonedDateTimeObservable.subscribe(s -> System.out.printf("S2: %s\n", s));
+    }
+
+    @Test
+    public void testSameDeferWithoutDefer() throws InterruptedException {
+        Observable<ZonedDateTime> zonedDateTimeObservable = Observable.just(ZonedDateTime.now());
+
+        zonedDateTimeObservable.subscribe(s -> System.out.printf("S1: %s\n", s));
+
+        Thread.sleep(4000);
+
+        zonedDateTimeObservable.subscribe(s -> System.out.printf("S2: %s\n", s));
+    }
+
+    //1. Interval of 1 second (This is the source observable)
+    //2. Create 2 forks
+    //     a. multiply each element by 2 and subscribe
+    //     b. filter all the odd numbers and subscribe
+    //3. Put Thread.sleep(10000) at the end
+    //4. Use debug and doOnNext where you feel it is necessary
 }
