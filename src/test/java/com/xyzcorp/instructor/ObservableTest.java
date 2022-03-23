a9be0b5711d1621d6e478ece81a0d90f736ea16f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-03-23 14:52:02 -0600

    Update MyObservable

---
+++ src/test/java/com/xyzcorp/instructor/ObservableTest.java
@@ -0,0 +1,52 @@
+package com.xyzcorp.instructor;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import org.junit.Test;
+
+public class ObservableTest {
+
+    @Test
+    public void testBasicObservable() {
+
+        Observable<Long> longObservable =
+            Observable.create(new ObservableOnSubscribe<Long>() {
+            @Override
+            public void subscribe
+                (@NonNull ObservableEmitter<Long> emitter) throws Exception {
+                emitter.onNext(100L);
+                emitter.onNext(250L);
+                emitter.onNext(440L);
+            }
+        });
+
+        longObservable.subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Long aLong) {
+                System.out.printf("S1 (onNext): %d [%s]\n",
+                    aLong, Thread.currentThread());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.printf("S1 (onError): %s [%s]\n",
+                    e.getMessage(), Thread.currentThread());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.printf("S1 (onComplete): [%s]\n",
+                    Thread.currentThread());
+            }
+        });
+    }
+}
